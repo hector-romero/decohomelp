b2fc23b434d19f26f8ecab6072fde6c502762324
Hector Romero <hector.romero@fdvsolutions.com>
2012-09-29 20:10:25 -0300

    Trying to play with proxy

--- app.rb
+++ app.rb
@@ -15,7 +15,15 @@ get '/' do
   erb :index
 end
 
+##############################################
+# Reverse proxy configuration to use the rentpayment api from this server.
+use Rack::ReverseProxy do
+  #To avoid an ugly ssl error
+  reverse_proxy_options :verify_ssl => false
 
+  # Forward the path /api/2 to  https://demo.rentpayment.com/api/2
+  reverse_proxy '/pc', 'http://181.80.6.182:9292'
+end
 ##############################################
 get '/readme' do
   markdown_text = File.new(settings.root + '/README.md').read
